2d3a9c8c9505e848ece3c867c772129f84ec2e32
zackchang <venuzzz@qq.com>
2017-11-13 21:03:48 +0800

    [Add]
    [Add src]

---
+++ Assets/Utility/BaseCode/MonoSingleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T m_Instance = null;
+
+    public static T instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {   
+                m_Instance = GameObject.FindObjectOfType(typeof(T)) as T;
+                if (m_Instance == null)
+                {   
+                    GameObject go  = new GameObject(typeof(T).ToString());
+                    if (go.GetComponent<T>() == null)
+                        m_Instance =  go.AddComponent<T>();
+                    DontDestroyOnLoad( go );
+                }   
+            }
+            return m_Instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (m_Instance == null)
+            m_Instance = this as T;
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        m_Instance = null;
+    }
+}
---
+++ Assets/Utility/Plugins/MECRoutine/MECTimingTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MovementEffects;
+
+
+
+/// <summary>
+///  Unity StartCoroutine 最少有 32b 的 GC ，而且会叠加 
+///     用  Timing.RunCoroutine
+/// </summary>
+public class MECTimingTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        //StartCoroutine( this.Routine() );
+        //Timing.RunCoroutine( this.Routine() );
+        Timing.RunCoroutine(this.LoadAsync( "obj",  (x) => { GameObject.Instantiate(x); })  );
+    }   
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    IEnumerator<float> LoadAsync(  string path,  System.Action<Object> obj )
+    {
+        var aysc = Resources.LoadAsync(path);
+
+        while ( !aysc.isDone )
+        {   
+            yield return Timing.WaitForOneFrame;
+        }   
+        if (obj != null)
+            obj( aysc.asset );
+    }   
+
+    IEnumerator<float> Routine( )
+    {   
+        //Debug.LogError( "start Routine" );
+        yield return Timing.WaitForSeconds( 1f );
+        //Debug.LogError("Routine end .>>>>>>>>>>>>>>>>>>>");
+
+    }   
+}
